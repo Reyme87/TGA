98d16c8470599a4e3a7b98395edf3476d8d4e81c
Reyme87 <astafev.ivan.0505@mail.ru>
2025-07-13 15:44:56 +0300

    Добавление основных коллекций элементов и методов их инициализации

--- TGA/ViewModels/MainViewModel.cs
+++ TGA/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TGA.Models;
 using TGA.ViewModels.Base;
+using TL;
 
 namespace TGA.ViewModels
 {
@@ -36,22 +38,66 @@ namespace TGA.ViewModels
 
         private static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "TextClassificationModel.zip");
 
-        private Classifier _classifier;
-        public Classifier Classifier
+        private TgParser _parser;
+        public TgParser Parser
         {
-            get => _classifier;
+            get => _parser;
             set
             {
-                Set(ref _classifier, value);
+                Set(ref _parser, value);
             }
         }
-        
+
+        private ChannelModel _selectedChannel;
+        public ChannelModel SelectedChannel
+        {
+            get => _selectedChannel;
+            set
+            {
+                Set(ref _selectedChannel, value);
+                if(_selectedChannel != null)
+                {
+                    CollectMessages();
+                }
+            }
+        }
+
         #endregion
 
-        public MainViewModel() 
+        public MainViewModel()
         {
-            Classifier = new Classifier();
-            Classifier.LoadModel(_modelPath);
+            Parser = new TgParser(Config);
+            CollectChats();
+
+            //Messages = new ObservableCollection<MessageData>();
+            //// Заполните данными для теста
+            //Messages.Add(new MessageData { Category = "Тест", Content = "Пример сообщения", Views = 100, TotalReactionsCount = 10 });
+        }
+
+        private async void CollectChats()
+        {
+            Channels = await Parser.GetChatsList();
+        }
+
+        private async void CollectMessages()
+        {
+            Messages = await Parser.ParseMessages(SelectedChannel.Id, 100);
+        }
+
+        private string Config(string what)
+        {
+            switch (what)
+            {
+                case "api_id": return Environment.GetEnvironmentVariable("api_id");
+                case "api_hash": return Environment.GetEnvironmentVariable("api_hash"); ;
+                case "phone_number": return Environment.GetEnvironmentVariable("phone_number");
+                case "verification_code":
+                    return Interaction.InputBox("Verification code:");
+                case "password":
+                    return Interaction.InputBox("Enter 2FA password");
+                case "session_pathname": return "session.dat";
+                default: return null;
+            }
         }
     }
 }
