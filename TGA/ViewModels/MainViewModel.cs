f506c12af7f15a098d2c589c40c4e2f2cd2e0d67
Reyme87 <astafev.ivan.0505@mail.ru>
2025-07-14 21:06:55 +0300

    Создание команд. Привязка к элементам управления.

--- TGA/ViewModels/MainViewModel.cs
+++ TGA/ViewModels/MainViewModel.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using TGA.Commands;
 using TGA.Models;
 using TGA.ViewModels.Base;
-using TL;
 
 namespace TGA.ViewModels
 {
@@ -16,8 +15,13 @@ namespace TGA.ViewModels
     {
         #region Коллекции элементов
 
+        private readonly static string _outputDir = "TelegramData";
+        private Visibility _isVisible;
+        //private static Client _client;
         private ObservableCollection<ChannelModel> _channels;
         private ObservableCollection<MessageData> _messages;
+        private string _phoneNumber = "";
+        private string _userName;
 
         public ObservableCollection<ChannelModel> Channels
         {
@@ -38,6 +42,14 @@ namespace TGA.ViewModels
 
         private static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "TextClassificationModel.zip");
 
+        public Visibility IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                Set(ref _isVisible, value);
+            }
+        }
         private TgParser _parser;
         public TgParser Parser
         {
@@ -55,23 +67,77 @@ namespace TGA.ViewModels
             set
             {
                 Set(ref _selectedChannel, value);
-                if(_selectedChannel != null)
+                if (_selectedChannel != null)
                 {
                     CollectMessages();
                 }
             }
         }
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                Set(ref _phoneNumber, value);
+            }
+        }
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                Set(ref _userName, value);
+            }
+        }
+
+        #endregion
+
+        #region Команды
+
+        #region ExportDataCommand
+
+        public ICommand ExportDataCommand { get; }
+
+        public async void OnExportDataCommandExecuted(object p)
+        {
+            Directory.CreateDirectory(_outputDir);
+
+            string tempTitle = SelectedChannel.Name;
+            string pattern = @"\W";
+            string result = Regex.Replace(tempTitle, "[^.\\w.\\s+]", "").Trim();
+
+            var path = Path.Combine(_outputDir, result);
+            Directory.CreateDirectory(path);
+            var fileName = Path.Combine(path, $"chat_{result}_{DateTime.Now:yyyy_MM_dd_HH_mm_ss}.json");
+            await File.WriteAllTextAsync(fileName, JsonConvert.SerializeObject(Messages.Reverse(), Formatting.Indented));
+        }
+
+        public bool CanExportDataCommandExecute(object p)
+        {
+            if (!Equals(SelectedChannel, null))
+            {
+                IsVisible = Visibility.Visible;
+                return true;
+            }
+            IsVisible = Visibility.Hidden;
+            return false;
+        }
+
+        #endregion
 
         #endregion
 
         public MainViewModel()
         {
-            Parser = new TgParser(Config);
-            CollectChats();
+            #region Команды
 
-            //Messages = new ObservableCollection<MessageData>();
-            //// Заполните данными для теста
-            //Messages.Add(new MessageData { Category = "Тест", Content = "Пример сообщения", Views = 100, TotalReactionsCount = 10 });
+            ExportDataCommand = new RelayCommand(OnExportDataCommandExecuted, CanExportDataCommandExecute);
+
+            #endregion
+
+            Parser = new TgParser(Config);
+            GetUsername();
+            CollectChats();
         }
 
         private async void CollectChats()
@@ -84,20 +150,62 @@ namespace TGA.ViewModels
             Messages = await Parser.ParseMessages(SelectedChannel.Id, 100);
         }
 
+        private async void GetUsername()
+        {
+            UserName = await Parser.GetUserName();
+        }
+
         private string Config(string what)
         {
             switch (what)
             {
                 case "api_id": return Environment.GetEnvironmentVariable("api_id");
-                case "api_hash": return Environment.GetEnvironmentVariable("api_hash"); ;
-                case "phone_number": return Environment.GetEnvironmentVariable("phone_number");
+                case "api_hash": return Environment.GetEnvironmentVariable("api_hash");
+                case "phone_number":
+                    if (GetInfo("phone.json") == null)
+                    {
+                        _phoneNumber = Interaction.InputBox("Введите номер телефона с +:");
+                        LoadInfoAsync(_phoneNumber, "phone.json");
+                        return _phoneNumber;
+                    }
+                    else
+                    {
+                        return GetInfo("phone.json");
+                    }
                 case "verification_code":
-                    return Interaction.InputBox("Verification code:");
+                    return Interaction.InputBox("Введите код верификации:");
                 case "password":
-                    return Interaction.InputBox("Enter 2FA password");
+                    return Interaction.InputBox("Введите 2FA пароль");
                 case "session_pathname": return "session.dat";
                 default: return null;
             }
+        }
+
+        static public async void LoadInfoAsync(string value, string fileName)
+        {
+            string json = JsonConvert.SerializeObject(value, Formatting.Indented);
+            await File.WriteAllTextAsync(fileName, json);
+        }
+
+        static public string GetInfo(string fileName)
+        {
+            string? number = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                if (fileInfo.Length != 0)
+                {
+                    try
+                    {
+                        number = System.Text.Json.JsonSerializer.Deserialize<string>(fs);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error occured while reading the data!");
+                    }
+                }
+            }
+            return number;
         }
     }
 }
